f9ed9bfb5df813e52a14b26556d9bfc07241ff87
ankitkaushal8292.in <ankitkaushal8292@gmail.com>
2025-12-28 23:38:03 +0530

    Initial commit

---
+++ HookPractice/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+let [counter,setCounter]=useState(15)
+  const addValue=()=>{
+    if(counter<=20){
+setCounter(counter+1)
+    }
+  }
+
+  const removeValue=()=>{
+    if(counter>0){
+    setCounter(counter-1)
+    }
+  }
+  return (
+    <>
+      <div>
+       
+        <h1>Counter Project</h1>
+        <h2>Counter Value {counter}</h2>
+        <button onClick={addValue}>Add Value</button>
+
+        <button onClick={removeValue}>Remove Value</button>
+      </div>
+     
+      
+    </>
+  )
+}
+
+export default App
